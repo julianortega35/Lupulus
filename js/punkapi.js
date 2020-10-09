70e317eabd6fa488eb2c740df8c040cf42d8fed9
julianortega <julianortega@MacBook-de-julianortega.local>
2020-10-09 16:47:34 +0200

    style

--- js/punkapi.js
+++ js/punkapi.js
@@ -1,63 +1,47 @@
-'use strict';
-window.addEventListener ("resize", () => {
-  window.scrollTo (0, document.body.scrollHeight)
-})
+"use strict";
+window.addEventListener("resize", () => {
+  window.scrollTo(0, document.body.scrollHeight);
+});
 
 function getRandomBeers() {
-
-
   const beerlist = document.querySelector(".mybeers");
- beerlist.innerHTML = "";
-  
- 
+  beerlist.innerHTML = "";
 
   fetch("https://api.punkapi.com/v2/beers")
     .then((response) => {
       return response.json();
     })
     .then((data) => {
-      console.log(data)
-      let randomBeers = []
+      console.log(data);
+      let randomBeers = [];
 
       for (let i = 0; i < 1; i++) {
-
-        const randomIndex = Math.floor((Math.random() * data.length));
-
+        const randomIndex = Math.floor(Math.random() * data.length);
 
         const spliceArray = data.splice(randomIndex, 1);
-        const randomBeer = spliceArray[0]
+        const randomBeer = spliceArray[0];
         randomBeers.push(randomBeer);
-
-
       }
-
 
       randomBeers.forEach((element) => {
         const div = document.createElement("div");
-        div.classList.add("one-beer")
+        div.classList.add("one-beer");
         div.innerHTML = `
-    <p>${element.name}</p>
-    <img src="${element.image_url}"/>
-    <p><br>
-    ${element.tagline}</p>
-  `
+        <p>${element.name}</p>
+        <img src="${element.image_url}"/>
+        <p><br>
+        ${element.tagline}</p>`;
         beerlist.appendChild(div);
-      })
-    })
-  
-  
-}  
+      });
+    });
+}
 getRandomBeers();
 
-
-var button = document.createElement("button");
-button.classList.add("get-beers-button")
+const button = document.createElement("button");
+button.classList.add("get-beers-button");
 button.innerHTML = "Get beers!";
 
-
-var body = document.getElementsByTagName("body")[0];
+const body = document.getElementsByTagName("body")[0];
 body.appendChild(button);
 
-
 button.addEventListener("click", getRandomBeers);
-
